9e046927da4d4f66fba80a0b07260057fc462fde
Dmitrij Kuropatkin <dmitrijfotowork@mail.ru>
2021-08-11 20:03:20 +0400

    Выполнил практическое занятие №4

---
+++ Kuropatkin_Dmitrij_dz_4/utils.py
@@ -0,0 +1,10 @@
+from requests import get, utils
+
+response = utils.get_unicode_from_response(get("http://www.cbr.ru/scripts/XML_daily.asp"))
+
+
+def currency_rates(code):
+    cont = response.split("<Valute ID=")
+    for i in cont:
+        if code.upper() in i:
+            return float(i.replace("/", "").split("<Value>")[-2].replace(",", "."))
